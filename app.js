bbb511cd81153c290f632e01be96e34a8dd5e3b7
MatthewShurtz <59268254+MatthewShurtz@users.noreply.github.com>
2020-12-16 13:23:39 -0600

    New files added

---
+++ app.js
@@ -0,0 +1,15 @@
+// Array for books
+let mattLibrary = [];
+
+// Selectors
+let addBookBtn = document.getElementById("add-book-btn");
+let titleList = document.getElementById("titles");
+let authorList = document.getElementById("authors");
+let pagesList = document.getElementById("pages");
+
+// Function to add books on the UI
+function addBook() {
+  let newTitleLi = document.createElement("li");
+  let newAuthorLi = document.createElement("li");
+  let newPagesLi = document.createElement("li");
+} 
